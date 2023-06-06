152701785d8235498a8db1178eeed39cea46b2a0
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-06-06 20:37:35 +0000

    Fix url typo in the reddit api url

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,8 +10,8 @@ def number_of_subscribers(subreddit):
     Gets all the subscribers from the subreddit passed as an argument
     """
     headers = {'User-Agent': 'Custom'}
-    url = 'https://www/reddit.com/r/{}/about.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = requests.get(url, headers=headers)
-    if response:
+    if response.status_code == 200:
         return response.json().get('data')['subscribers']
     return 0
