be64ccab629306d474d4dfae743f2c67f133f3ef
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-06-06 20:23:14 +0000

    get all subsribers from a subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,15 +3,15 @@
 Get all the subscribers from a given subreddit
 """
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
     """
     Gets all the subscribers from the subreddit passed as an argument
     """
-    url = 'https://www/reddit.com/r/{}/about'.format(subreddit)
-    response = requests.get(url)
+    headers = {'User-Agent': 'Custom'}
+    url = 'https://www/reddit.com/r/{}/about.json'.format(subreddit)
+    response = requests.get(url, headers=headers)
     if response:
         return response.json().get('data')['subscribers']
     return 0
